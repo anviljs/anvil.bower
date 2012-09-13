0f5f01e41e92b2553178bc0273983be1dd1f24fe
Alex Robson <asrobson@gmail.com>
2012-09-13 00:34:23 -0500

    Initial checkin

---
+++ README.md
@@ -0,0 +1,10 @@
+## Anvil Bower Plugin
+
+This plugin adds bower commands to anvil (somewhat redundant, you could just use bower...)
+
+### Warning
+Bower and TJ Holowaychuck's Component tools are incompatible. If you're installing components using Holowaychuck's component, then pretty much all the bower commands break. This has nothing to do with anvil; please don't report the issue to me :)
+
+## Installation
+
+anvil install anvil.bower
---
+++ src/bower.js
@@ -0,0 +1,44 @@
+var bower = require( "bower" );
+var path = require( "path" );
+
+module.exports = function( _, anvil ) {
+
+	var prefix = "bower:";
+	return anvil.plugin( {
+		name: "anvil.bower",
+		commander: [
+			[ prefix + "install [value]", "Install a package via bower" ],
+			[ prefix + "uninstall [value]", "Uninstall a package via bower" ],
+			[ prefix + "search [value]", "Search bower for a package" ],
+			[ prefix + "list", "List installed bower packages" ]
+		],
+
+		configure: function( config, command, done ) {
+			var commands = [ "install", "uninstall", "search", "list" ],
+				key = _.find( commands, function( c ) {
+					return command[ prefix + c ];
+				} );
+			if( key ) {
+				this.command( key, command[ prefix + key ] );
+			} else {
+				done();
+			}
+		},
+
+		command: function( command, value ) {
+			var self = this,
+				stop = function( data ) {
+					console.log( data );
+					anvil.events.raise( "all.stop", 0 );
+				};
+			bower.commands[ command ].line( process.argv )
+				.on('data', self.log )
+				.on('end', stop )
+				.on('error', stop );
+		},
+
+		log: function( data ) {
+			if( data ) { console.log( data ); }
+		}
+	} );
+};
