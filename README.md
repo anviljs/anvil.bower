23782d629d19992e78ca7c194fde3c30be34c482
Alex Robson <asrobson@gmail.com>
2013-01-13 17:00:47 -0600

    Add missing license, header and npmignore files. Update to support dependencies

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
 ## Anvil Bower Plugin
 
-This plugin adds bower commands to anvil (somewhat redundant, you could just use bower...)
+Adds bower commands to anvil (somewhat redundant, you could just use bower...) and attempts to move resources from the package into package specific subfolders under ./ext in the output directory.
 
 ### Warning
 Bower and TJ Holowaychuck's Component tools are incompatible. If you're installing components using Holowaychuck's component, then pretty much all the bower commands break. This has nothing to do with anvil; please don't report the issue to me :)
+
+## Command Line
+Really the only advantage here is not having to have bower installed globally. To issue any command, you just type "anvil bower [command] [args]" and anvil will pass the task on.
+
+## Dependencies
+If you want Anvil to auto-install your client dependencies, you can list them in your build file like this:
+
+```javascript
+{
+	"anvil.bower": {
+		"dependencies": [
+			"backbone",
+			"underscore"
+		]
+	}
+}
+
+This extension will check for the presence of these packages before each build and ensure they've been installed.
+
+## Problems
+Unfortunately not everyone follows the same conventions in their package.json or component.json files so there's no predictable way for anvil to know where to find the component's resources. If there's no folder under lib/ext for a component, it's because the author failed to specify; please feel free to let them know they're doing it wrong.
 
 ## Installation
 
--- src/bower.js
+++ src/bower.js
@@ -1,44 +1,108 @@
-var bower = require( "bower" );
+var bower;
 var path = require( "path" );
 
 module.exports = function( _, anvil ) {
 
-	var prefix = "bower:";
-	return anvil.plugin( {
-		name: "anvil.bower",
-		commander: [
-			[ prefix + "install [value]", "Install a package via bower" ],
-			[ prefix + "uninstall [value]", "Uninstall a package via bower" ],
-			[ prefix + "search [value]", "Search bower for a package" ],
-			[ prefix + "list", "List installed bower packages" ]
-		],
+	var invoke = function( command, args, done ) {
+		if( !bower ) {
+			bower = require( "bower" );
+		}
+		var call = bower.commands[ command ];
+		if( call ) {
+			args = args ? [ args ] : [];
+			call( args )
+				.on( 'data', anvil.log.event )
+				.on( 'error', anvil.log.error )
+				.on( 'end', function() { done(); } );
+		} else {
+			anvil.error.log( "Bower does not support command: '" + command + "'" );
+		}
+	};
 
-		configure: function( config, command, done ) {
-			var commands = [ "install", "uninstall", "search", "list" ],
-				key = _.find( commands, function( c ) {
-					return command[ prefix + c ];
-				} );
-			if( key ) {
-				this.command( key, command[ prefix + key ] );
-			} else {
-				done();
+	anvil.command( {
+		name: "anvil.bower",
+		commander: {
+			"bower <command> [value]": {
+				description: "Install, uninstall or search for a package",
+				action: "process"
 			}
 		},
 
-		command: function( command, value ) {
-			var self = this,
-				stop = function( data ) {
-					console.log( data );
-					anvil.raise( "all.stop", 0 );
-				};
-			bower.commands[ command ].line( process.argv )
-				.on('data', self.log )
-				.on('end', stop )
-				.on('error', stop );
+		configure: function( config, command, done ) {
+			done();
 		},
 
-		log: function( data ) {
-			if( data ) { console.log( data ); }
+		process: function( command, value, options, done ) {
+			invoke( command, value, done );
 		}
+	} );
+
+	anvil.plugin( {
+		name: "bower.build",
+		activities: [ "pre-build", "identify" ],
+		run: function( done, activity ) {
+			this[ activity ]( done );
+		},
+		identify: function( done ) {
+			var componentsBase = path.resolve( "./components" ),
+				exists = anvil.fs.pathExists( componentsBase );
+			if( !exists ) {
+				done();
+			} else {
+				anvil.fs.getFiles( componentsBase, componentsBase, function( files, directories ) {
+					var metadata = [];
+					_.each( directories, function( directory ) {
+						var relativePath = directory.replace( componentsBase, "" );
+							componentPath = anvil.fs.buildPath( [ directory, "component.json" ] ),
+							packagePath = anvil.fs.buildPath( [ directory, "package.json" ] ),
+							component = anvil.fs.pathExists( componentPath ) ? require( componentPath ) : {},
+							pack = anvil.fs.pathExists( packagePath ) ? require( packagePath ) : {},
+							componentName = path.basename( directory );
+						var files = [];
+						if( component.main ) {
+							files.push( component.main );
+						}
+						if( pack.main ) {
+							files.push( pack.main );
+						}
+						if( component.styles ) {
+							files = files.concat( component.styles );
+						}
+						if( component.scripts ) {
+							files = files.concat( component.scripts );
+						}
+						var data = _.map( files, function( file ) {
+							file = anvil.fs.buildPath( [ directory, file ] );
+							var data = anvil.fs.buildFileData(
+									path.dirname( file ),
+									anvil.fs.buildPath( [ anvil.config.working, "src", "ext", componentName ] ),
+									file );
+							data.relativePath = anvil.fs.buildPath( [ "ext", relativePath ] );
+							return data;
+						} );
+						metadata = metadata.concat( data );
+					} );
+					metadata = _.uniq( metadata, false, function( x ) { return x.fullPath; } );
+					anvil.project.files = anvil.project.files.concat( metadata );
+					done();
+				}, [], 0 );
+			}
+		},
+		"pre-build": function( done ) {
+			anvil.extensions.tasks[ "bower-dependencies" ].run( [], done );
+		}
+	} );
+
+	anvil.task( "bower-dependencies", "install bower dependencies found in the build file", function( done ) {
+		var config = anvil.config[ "anvil.bower" ],
+			dependencies = config ? config.dependencies : [];
+		anvil.scheduler.parallel( dependencies, function( dependency, done ) {
+			var exists = anvil.fs.pathExists( [ "./components", dependency ] );
+			if( !exists ) {
+				invoke( "install", dependency, done );
+			} else {
+				done();
+			}
+		}, function() { done(); } );
 	} );
 };
